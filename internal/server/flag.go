0e25add6c3b05ca51d604671aeba53fa45bf9621
Alex Polianski <a.palianski@lumos.mobi>
2025-02-21 12:09:20 +0300

    test

--- internal/server/flag.go
+++ internal/server/flag.go
@@ -7,6 +7,13 @@ import (
 
 func ParseFlags() (Config, error) {
 	config := Config{}
+	flag.StringVar(&config.ServerAddress, "a", "localhost:8080",
+		"address and port to run server [default:localhost:8080]") // RUN_ADDRESS
+	flag.StringVar(&config.DatabaseURI, "d", "",
+		"uri for database [default:]") // RUN_ADDRESS
+	flag.StringVar(&config.AccurakSystemAddres, "r", "localhost:8091",
+		"addres for connect accures sysem [default:localhost:8091]") // RUN_ADDRESS
+	flag.Parse()
 	if envServerAdderess := os.Getenv("RUN_ADDRESS"); envServerAdderess != "" {
 		config.ServerAddress = envServerAdderess
 	}
@@ -16,13 +23,6 @@ func ParseFlags() (Config, error) {
 	if envAccurakSystemAddres := os.Getenv("ACCRUAL_SYSTEM_ADDRESS"); envAccurakSystemAddres != "" {
 		config.AccurakSystemAddres = envAccurakSystemAddres
 	}
-	flag.StringVar(&config.ServerAddress, "a", "localhost:8080",
-		"address and port to run server [default:localhost:8080]") // RUN_ADDRESS
-	flag.StringVar(&config.DatabaseURI, "d", "",
-		"uri for database [default:]") // RUN_ADDRESS
-	flag.StringVar(&config.AccurakSystemAddres, "r", "localhost:8091",
-		"addres for connect accures sysem [default:localhost:8091]") // RUN_ADDRESS
-	flag.Parse()
 	config.DatabaseURI = "host=localhost port=5430 user=gopher password=qwerty dbname=postgres_db sslmode=disable"
 	return config, nil
 }
