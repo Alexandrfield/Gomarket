3947e93b5429955245ef137e6fbd5f0742fb2b90
DESKTOP-T7ARDPC\User <alekpoalan@gmail.com>
2025-02-28 00:43:55 +0300

    t

--- internal/handle/midlware.go
+++ internal/handle/midlware.go
@@ -47,6 +47,8 @@ func (han *ServiceHandler) WithLogging(h http.HandlerFunc) http.HandlerFunc {
 		}
 		han.Logger.Debugf("Request: %s", r)
 		tokenString := r.Header.Get("Authorization")
+		han.Logger.Debugf("tokenString:%s", tokenString)
+
 		if tokenString == "" {
 			w.WriteHeader(http.StatusUnauthorized)
 			han.Logger.Debugf("Missing authorization header")
